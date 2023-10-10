cc001987cabfa0e764a0ba77445fa5a157e97afb
Errytagedesign <errytage@gmail.com>
2023-10-10 15:10:58 +0100

    Add librarymanager

---
+++ library_manager.rb
@@ -0,0 +1,21 @@
+require_relative 'person_class'
+require_relative 'book_class'
+require_relative 'rental_class'
+
+class LibraryManager
+  attr_reader :people, :books, :rentals
+
+  def initialize
+    @people = []
+    @books = []
+    @rentals = []
+  end
+
+  def create_person(age, name, specialization = nil)
+    person = specialization ? Teacher.new(specialization, age, name) : Student.new(age, name)
+    @people << person
+    person
+  end
+
+
+end
