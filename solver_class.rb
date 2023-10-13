02eb62be926c6c5dd4b96e723a1ff3b232b17f2d
zbahati <bahatizirimwabagabo@gmai.com>
2023-10-13 11:51:41 +0200

    Fix the first check if the factorial number given is 0 should return the 1

--- solver_class.rb
+++ solver_class.rb
@@ -1,2 +1,16 @@
 class Solver
+  def factorial(n)
+    if n < 0
+      raise ArgumentError 'Input should be non negative numbers'
+    end
+    result = 1
+
+    # (1..n).each do |i|
+    #   result * = i;
+    # end
+    # result
+  end
 end
+
+solver = Solver.new
+puts solver.factorial(0)
--- test/solver_tdd.rb
+++ test/solver_tdd.rb
@@ -4,7 +4,7 @@ describe Solver do
 
   describe '#factorial' do
     it 'returns 1 when given 0' do
-      expect(factorial.new.factorial(0).to eq(1))
+      expect(Solver.new.factorial(0)).to eq(1)
     end
   end
 end
