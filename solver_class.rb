a3b5733178f09ab4228d14be0ee3f3272e6c447a
Errytagedesign <errytage@gmail.com>
2023-10-13 14:12:39 +0100

    Add fuzzbuzz

--- solver_class.rb
+++ solver_class.rb
@@ -1,18 +1,30 @@
 class Solver
-  def factorial(n)
-    if n < 0
+  def factorial(val)
+    if val.negative?
       raise ArgumentError, 'Input should be non negative number'
     end
+
     result = 1
 
-    (1..n).each do |i|
+    (1..val).each do |i|
       result *= i
     end
     result
   end
 
-  def reverse (word)
+  def reverse(word)
     word.reverse
   end
 
+  def fuzzbuzz(number)
+    if (number % 5).zero? && (number % 3).zero?
+      'fuzzbuzz'
+    elsif (number % 5).zero?
+      'buzz'
+    elsif (number % 3).zero?
+      'fuzz'
+    else
+      number.to_i
+    end
+  end
 end
