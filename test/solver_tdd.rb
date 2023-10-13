04111d811e88f4fe2a5d5eca4c1eb8f2e9e9a4f3
zbahati <bahatizirimwabagabo@gmai.com>
2023-10-13 11:41:04 +0200

    Now after difining the first test for the factorial that return 1 when given zero
     - the test files by undefined local variable factorial in Solver

--- test/solver_tdd.rb
+++ test/solver_tdd.rb
@@ -2,5 +2,9 @@ require 'rspec'
 require_relative '../solver_class'
 describe Solver do
 
-
+  describe '#factorial' do
+    it 'returns 1 when given 0' do
+      expect(factorial.new.factorial(0).to eq(1))
+    end
+  end
 end
